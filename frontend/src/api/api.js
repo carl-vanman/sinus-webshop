a06b39058b0faed5a24452973f8fee6c3d460c6a
carl-vanman <70966062+carl-vanman@users.noreply.github.com>
2021-02-25 12:14:16 +0100

    Parkodt

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,2 +1,14 @@
 import axios from 'axios';
 
+const BASE_URL = "http://localhost:5000/api/"
+export const PRODUCTS_URL = `${BASE_URL}products`
+
+export const get = async (url) => {
+
+    try {
+        const response = await axios.get(url);
+        return response;
+    } catch (error) {
+        console.log(error)
+    }
+}
--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -1,13 +1,38 @@
 <template>
-  <p>produktlista</p>
+  <div class="productBoard">
+    <div v-for="product in products" :key="product._id">
+      <Product :product="product" />
+      <button class="lägg i cart"> img<!-- <img src="#" alt="..."> --></button>
+    </div>
+  </div>
 </template>
 
 <script>
+import Product from '../components/Product.vue'
+import {PRODUCTS_URL, get} from '@/api/api.js'
+
 export default {
+  components: { 
+    Product, 
+    },
+    data() {
+      return {
+        products: []
+      }
+    },
+    async created() {
+      const response = await get(PRODUCTS_URL)
+      this.products = response.data;
+    }
 
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+  .productBoard {
+    width: 100vw;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 30px;
+  }
 </style>
