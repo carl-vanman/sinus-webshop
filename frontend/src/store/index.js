e8d91beeed1feb1dec2ce7f2fa173a94d090363d
jessicahaara <jessica.haara@iths.se>
2021-02-26 11:35:34 +0100

    store changes
    Ändrade om i kod i actions och mutations så att mutations blev enklare

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -29,53 +29,82 @@ export default new Vuex.Store({
     PRODUCTS_LIST(state, products) {
       state.products = products
     },
-    /* om tid finns, hitta en snyggare lösning */
-    ADD_PRODUCT(state, productObj) {
-      state.cartItems++
-      state.cartPrice += productObj.price
-      let exists = false
-      for (let i = 0; i < state.cart.length; i++) {
-        if (state.cart[i]._id === productObj._id) {
-          exists = true
-          console.log('finns')
-          state.cart[i].amount++
-          break;
 
-        }
-      }
-      if (exists === false) {
-        productObj.amount = 1
-        state.cart.push(productObj)
-        console.log(state.cart)
-      }
+    ADD_PRODUCT(state, productObj) {
+      state.cart.push(productObj)
+      console.log(state.cart)
     },
 
     REMOVE_FROM_CART(state, index) {
+      state.cart.splice(index, 1)
+    },
+
+    increaseAmount(state, index) {
+      state.cart[index].amount++
+    },
+
+    decreaseAmount(state, index) {
+      state.cart[index].amount--
+    },
+
+    ADD_CART_ITEM(state) {
+      state.cartItems++
+    },
+
+    ADD_TO_PRICE(state, payload) {
+      state.cartPrice += payload.price
+    },
+
+    removeCartItem(state) {
+      state.cartItems--
+    },
+
+    removeFromPrice(state, index) {
       state.cartPrice -= state.cart[index].price
-      if (state.cart[index].amount === 1) {
-        state.cartItems--
-        state.cart.splice(index, 1)
-      } else {
-        state.cartItems--
-        state.cart[index].amount--
-      }
     }
   },
+
   actions: {
     async getProducts({ commit }, url) {
       const response = await get(url)
       const products = response.data;
       commit('PRODUCTS_LIST', products)
     },
-    async getProduct({ commit }, url) {
+
+    async getProduct(context, url) {
       const response = await get(url)
       const product = response.data;
-      commit('ADD_PRODUCT', product)
+
+      context.commit('ADD_CART_ITEM')
+      context.commit('ADD_TO_PRICE', product)
+
+      let exists = false
+
+      for (let i = 0; i < context.state.cart.length; i++) {
+        if (context.state.cart[i]._id === product._id) {
+          exists = true
+          console.log('finns')
+          context.commit('increaseAmount', i)
+          break;
+
+        }
+      }
+      if (exists === false) {
+        product.amount = 1
+        context.commit('ADD_PRODUCT', product)
+      }
     },
 
-    removeProduct({ commit }, index) {
-      commit('REMOVE_FROM_CART', index)
-    }
+    removeProduct(context, index) {
+      context.commit('removeFromPrice', index)
+      context.commit('removeCartItem')
+
+      if (context.state.cart[index].amount === 1) {
+        context.commit('REMOVE_FROM_CART', index)
+      } else {
+        context.commit('decreaseAmount', index)
+      }
+    },
   },
   modules: {
   },
