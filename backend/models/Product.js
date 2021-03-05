67c5a92a1f757d874b752eb3c2adc4daf92f6439
jessicahaara <jessica.haara@iths.se>
2021-03-05 11:45:01 +0100

    uppdaterade backend
    för att kunna ändra produktcategory

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,76 +1,76 @@
 const NeDB = require('nedb-promise')
-const products = new NeDB({filename:'database/products.db', autoload: true})
+const products = new NeDB({ filename: 'database/products.db', autoload: true })
 
 const Errors = require('../errors')
 
-const categorySort = (a,b) => a.category < b.category ? -1 : 1
+const categorySort = (a, b) => a.category < b.category ? -1 : 1
 
 
 module.exports = {
-    async find(params){
-        if(params.length){
-            return await products.find({_id: {$in:params}})
-        }else{
+    async find(params) {
+        if (params.length) {
+            return await products.find({ _id: { $in: params } })
+        } else {
             return await products.find(params)
         }
     },
 
-    async get(id){
-        return await products.findOne({_id:id})
+    async get(id) {
+        return await products.findOne({ _id: id })
     },
 
-    async all(){
-        try{
+    async all() {
+        try {
             let prods = await products.find({})
             prods = prods.sort(categorySort)
             return prods
 
-        }catch(error){
+        } catch (error) {
             console.log(error)
             return false
         }
     },
 
-    async create(body){
-        let {title, price, shortDesc, longDesc, imgFile} = body
+    async create(body) {
+        let { title, price, shortDesc, longDesc, imgFile, category } = body
         let serial = Date.now()
-        try{
-            const product = await products.insert({title, price, shortDesc, longDesc, imgFile, serial})
-            return {error: false, product}
-        }catch(err){
-            return {error: true, message:err}
+        try {
+            const product = await products.insert({ title, price, shortDesc, longDesc, imgFile, category, serial })
+            return { error: false, product }
+        } catch (err) {
+            return { error: true, message: err }
         }
     },
 
-    async update(productId, body){
-        try{
-            const keys = ['title', 'price', 'shortDesc', 'longDesc', 'imgFile'];
+    async update(productId, body) {
+        try {
+            const keys = ['title', 'price', 'shortDesc', 'longDesc', 'imgFile', 'category'];
             let patch = keys.reduce((acc, key) => {
-                if(body[key]){ acc[key] = body[key] }
+                if (body[key]) { acc[key] = body[key] }
                 return acc
             }, {})
-            
-            const numUpdated = await products.update({_id:productId}, patch)
 
-            if(numUpdated > 0){
-                const product = await products.findOne({_id:productId})
-                return {error: Errors.NO_ERROR, data: product}
-            }else{
-                return {error: Errors.NOT_FOUND}
+            const numUpdated = await products.update({ _id: productId }, patch)
+
+            if (numUpdated > 0) {
+                const product = await products.findOne({ _id: productId })
+                return { error: Errors.NO_ERROR, data: product }
+            } else {
+                return { error: Errors.NOT_FOUND }
             }
 
-        }catch(error){
-            return {error: Errors.INVALID_PARAMETERS, message:error}
+        } catch (error) {
+            return { error: Errors.INVALID_PARAMETERS, message: error }
         }
     },
 
-    async destroy(productId){
-        try{
-            await products.remove({_id:productId})
-            return {error:false}
-        }catch(error){
+    async destroy(productId) {
+        try {
+            await products.remove({ _id: productId })
+            return { error: false }
+        } catch (error) {
             console.log(error)
-            return {error:true}
+            return { error: true }
         }
     }
 
